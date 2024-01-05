534a9b5668f33a0c76efc3d8f9bf9c3ddc8595f5
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-05 11:13:06 +0700

    Update sp_FindCustomerOrdersByTimePeriod.sql

--- sp_customerOrder/sp_FindCustomerOrdersByTimePeriod.sql
+++ sp_customerOrder/sp_FindCustomerOrdersByTimePeriod.sql
@@ -1,8 +1,25 @@
 CREATE OR ALTER PROCEDURE FindCustomerOrdersByTimePeriod
-    @start_date DATE,
-    @end_date DATE
+    @start_date DATE = NULL,
+    @end_date DATE = NULL
 AS
 BEGIN
-    SELECT * FROM Customer_Order
-    WHERE order_date BETWEEN @start_date AND @end_date;
+    IF @start_date IS NULL AND @end_date IS NULL
+	BEGIN
+		SELECT * FROM Customer_Order
+	END;
+	ELSE IF @start_date IS NULL AND @end_date IS NOT NULL
+	BEGIN
+		SELECT * FROM Customer_Order
+		WHERE order_date <= @end_date
+	END;
+	ELSE IF @start_date IS NOT NULL AND @end_date IS NULL
+	BEGIN
+		SELECT * FROM Customer_Order
+		WHERE order_date >= @start_date
+	END;
+	ELSE
+	BEGIN
+		SELECT * FROM Customer_Order
+		WHERE order_date BETWEEN @start_date AND @end_date
+	END;
 END;
