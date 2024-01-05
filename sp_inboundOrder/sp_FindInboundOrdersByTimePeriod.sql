f98c35b58b51c7444094c3f8136cbcafcba2bc56
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-05 11:26:21 +0700

    Update sp_FindInboundOrdersByTimePeriod.sql

--- sp_inboundOrder/sp_FindInboundOrdersByTimePeriod.sql
+++ sp_inboundOrder/sp_FindInboundOrdersByTimePeriod.sql
@@ -1,8 +1,25 @@
 CREATE OR ALTER PROCEDURE FindInboundOrdersByTimePeriod
-    @start_date DATE,
-    @end_date DATE
+    @start_date DATE = NULL,
+    @end_date DATE = NULL
 AS
 BEGIN
-    SELECT * FROM Inbound_Order
-    WHERE order_date BETWEEN @start_date AND @end_date;
+    IF @start_date IS NULL AND @end_date IS NULL
+	BEGIN
+		SELECT * FROM Inbound_Order
+	END;
+	ELSE IF @start_date IS NULL AND @end_date IS NOT NULL
+	BEGIN
+		SELECT * FROM Inbound_Order
+		WHERE order_date <= @end_date
+	END;
+	ELSE IF @start_date IS NOT NULL AND @end_date IS NULL
+	BEGIN
+		SELECT * FROM Inbound_Order
+		WHERE order_date >= @start_date
+	END;
+	ELSE
+	BEGIN
+		SELECT * FROM Inbound_Order
+		WHERE order_date BETWEEN @start_date AND @end_date
+	END;
 END;
