8e9d85188216af5bbb4b2fe342c3855763675630
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-05 10:55:04 +0700

    Update sp_FindCustomerOrdersByStatus.sql

--- sp_customerOrder/sp_FindCustomerOrdersByStatus.sql
+++ sp_customerOrder/sp_FindCustomerOrdersByStatus.sql
@@ -1,7 +1,14 @@
 CREATE OR ALTER PROCEDURE FindCustomerOrdersByStatus
-    @status NVARCHAR(50)
+    @status NVARCHAR(50) = NULL
 AS
 BEGIN
-    SELECT * FROM Customer_Order
-    WHERE status = @status;
+    IF @status IS NULL
+	BEGIN
+		SELECT * FROM Customer_Order
+	END;
+	ELSE
+	BEGIN
+		SELECT * FROM Customer_Order
+		WHERE status LIKE '%' + @status + '%';
+	END;
 END;
