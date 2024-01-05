d999aaa1b5bfbe66ddd50e7afcdf89a00b28f4c2
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-05 10:44:34 +0700

    Update sp_FindProductsByCategoryName.sql

--- sp_product/sp_FindProductsByCategoryName.sql
+++ sp_product/sp_FindProductsByCategoryName.sql
@@ -1,9 +1,16 @@
 CREATE OR ALTER PROCEDURE FindProductsByCategoryName
-    @category_name NVARCHAR(50)
+    @category_name NVARCHAR(50) = NULL
 AS
 BEGIN
-    SELECT P.*
-    FROM Product P
-    INNER JOIN Product_category PC ON P.product_category_id = PC.product_category_id
-    WHERE PC.category_name = @category_name;
+    IF @category_name IS NULL
+	BEGIN
+		SELECT * FROM Product
+	END;
+	ELSE
+	BEGIN
+		SELECT P.*
+		FROM Product P
+		INNER JOIN Product_category PC ON P.product_category_id = PC.product_category_id
+		WHERE PC.category_name LIKE '%' + @category_name + '%';
+	END;
 END;
