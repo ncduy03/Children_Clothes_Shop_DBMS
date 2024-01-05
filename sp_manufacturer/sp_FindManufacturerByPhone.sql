8c815a97441b214142bd5157ca8cd0d7f14cd9a9
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-05 11:35:14 +0700

    Update sp_FindManufacturerByPhone.sql

--- sp_manufacturer/sp_FindManufacturerByPhone.sql
+++ sp_manufacturer/sp_FindManufacturerByPhone.sql
@@ -1,7 +1,14 @@
 CREATE OR ALTER PROCEDURE FindManufacturerByPhone
-    @searchPhone CHAR(10)
+    @searchPhone CHAR(10) = NULL
 AS
 BEGIN
-    SELECT * FROM Manufacturer
-    WHERE phone = @searchPhone;
+    IF @searchPhone IS NULL
+	BEGIN
+		SELECT * FROM Manufacturer
+	END;
+	ELSE
+	BEGIN
+		SELECT * FROM Manufacturer
+		WHERE phone LIKE '%' + @searchPhone + '%'
+	END;
 END;
