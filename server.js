2455e6922b9e546dc9abf46c4d8d7e35681ffd31
Ttdne <86353412+Ttdne@users.noreply.github.com>
2024-01-05 13:54:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ sql.connect(config, (err) => {
     }
 });
 
+app.get('/tongquan', (req, res) => {
+    res.render('tongquan.ejs');
+})
+
 app.route('/nhanvien')
     .post(async (req, res) => {
         const request = new sql.Request();
@@ -52,14 +56,12 @@ app.route('/nhanvien')
     })
 
 
-app.get('/tongquan', (req, res) => {
-    res.render('tongquan.ejs');
-})
 
-app.route('/doitac')
+
+app.route('/customer_order')
     .get(async (req, res) => {
-        const result = await sql.query(`SELECT * FROM Manufacturer`);
-        res.render('doitac', { dulieu: result.recordset });
+        const result = await sql.query(`SELECT * FROM Customer_Order`);
+        res.render('', { dulieu: result.recordset });
     })
     .post(async (req, res) => {
         const request = new sql.Request();
@@ -67,25 +69,49 @@ app.route('/doitac')
         const inputData2 = req.body.input_data2;
         if (inputData1) {
             request.input('InputData', sql.NVarChar, inputData1);
-            const result = await request.query(`EXEC FindManufacturerByName @InputData`);
-            res.render('doitac', { dulieu: result.recordset });
+            const result = await request.query(`EXEC FindCustomerOrdersByStatus @InputData`);
+            res.render('', { dulieu: result.recordset });
         }
         else if (inputData2) {
             request.input('InputData', sql.NVarChar, inputData2);
-            const result = await request.query(`EXEC FindManufacturerByPhone @InputData`);
+            const result = await request.query(`EXEC FindCustomerOrdersByTimePeriod @InputData`);
             console.log(result);
-            res.render('doitac', { dulieu: result.recordset });
+            res.render('', { dulieu: result.recordset });
         }
         else {
-            const result = await request.query(`SELECT * FROM Manufacturer`);
-            res.render('doitac', { dulieu: result.recordset });
+            const result = await request.query(`SELECT * FROM Customer_Order`);
+            res.render('', { dulieu: result.recordset });
         }
     });
 
 
-app.get('/giaodich', (req, res) => {
-    res.render('giaodich.ejs');
-})
+app.route('/INBOUND_ORDER')
+    .get(async (req, res) => {
+        const result = await sql.query(`SELECT * FROM Inbound_Order`);
+        res.render('', { dulieu: result.recordset });
+    })
+    .post(async (req, res) => {
+        const request = new sql.Request();
+        const inputData1 = req.body.input_data1;
+        const inputData2 = req.body.input_data2;
+        if (inputData1) {
+            request.input('InputData', sql.NVarChar, inputData1);
+            const result = await request.query(`EXEC FindInboundOrdersByStatus @InputData`);
+            res.render('', { dulieu: result.recordset });
+        }
+        else if (inputData2) {
+            request.input('InputData', sql.NVarChar, inputData2);
+            const result = await request.query(`EXEC FindInboundOrdersByTimePeriod @InputData`);
+            console.log(result);
+            res.render('', { dulieu: result.recordset });
+        }
+        else {
+            const result = await request.query(`SELECT * FROM Inbound_Order`);
+            res.render('', { dulieu: result.recordset });
+        }
+    });
+
+
 
 app.route('/kiemkho')
     .get(async (req, res) => {
@@ -121,6 +147,31 @@ app.route('/kiemkho')
         }
     });
 
+
+app.route('/doitac')
+    .post(async (req, res) => {
+        const request = new sql.Request();
+        const inputData1 = req.body.input_data1;
+        const inputData2 = req.body.input_data2;
+        if (inputData1) {
+            request.input('InputData', sql.NVarChar, inputData1);
+            const result = await request.query(`EXEC FindManufacturerByName @InputData`);
+            res.render('doitac', { dulieu: result.recordset });
+        }
+        else if (inputData2) {
+            request.input('InputData', sql.NVarChar, inputData2);
+            const result = await request.query(`EXEC FindManufacturerByPhone @InputData`);
+            res.render('doitac', { dulieu: result.recordset });
+        }
+        else {
+            const result = await request.query(`SELECT * FROM Manufacturer`);
+            res.render('doitac', { dulieu: result.recordset });
+        }
+    })
+    .get(async (req, res) => {
+        const result = await sql.query(`SELECT * FROM Manufacturer`);
+        res.render('doitac', { dulieu: result.recordset });
+    })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
