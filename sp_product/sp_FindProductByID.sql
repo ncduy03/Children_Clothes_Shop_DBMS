57ba26af4edc9e595778312e7103370bb43c6f7a
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-05 10:39:29 +0700

    Update sp_FindProductByID.sql

--- sp_product/sp_FindProductByID.sql
+++ sp_product/sp_FindProductByID.sql
@@ -1,7 +1,14 @@
 CREATE OR ALTER PROCEDURE FindProductByID
-    @product_id INT
+    @product_id INT = NULL
 AS
 BEGIN
-    SELECT * FROM Product
-    WHERE product_id = @product_id;
+    IF @product_id IS NULL
+	BEGIN
+		SELECT * FROM Product
+	END;
+	ELSE
+	BEGIN
+	SELECT * FROM Product
+	WHERE product_id = @product_id
+	END;
 END;
