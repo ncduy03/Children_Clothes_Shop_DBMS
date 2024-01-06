b0de758deb81a5689f9fb49f7860f742a1f6aac9
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-07 05:39:47 +0700

    Update sp_FindInboundOrdersByStatus.sql

--- sp_inboundOrder/sp_FindInboundOrdersByStatus.sql
+++ sp_inboundOrder/sp_FindInboundOrdersByStatus.sql
@@ -4,11 +4,14 @@ AS
 BEGIN
     IF @status IS NULL
 	BEGIN
-		SELECT * FROM Inbound_Order
+		SELECT IO.inbound_order_id, M.manufacturer_name, IO.order_date, IO.total_price, IO.status
+		FROM Inbound_Order IO, Manufacturer M
+		WHERE IO.manufacturer_id = M.manufacturer_id
 	END;
 	ELSE
 	BEGIN
-		SELECT * FROM Inbound_Order
-		WHERE status LIKE '%' + @status + '%'
+		SELECT IO.inbound_order_id, M.manufacturer_name, IO.order_date, IO.total_price, IO.status
+		FROM Inbound_Order IO, Manufacturer M
+		WHERE IO.manufacturer_id = M.manufacturer_id AND STATUS LIKE '%' + @status + '%'
 	END;
 END;
