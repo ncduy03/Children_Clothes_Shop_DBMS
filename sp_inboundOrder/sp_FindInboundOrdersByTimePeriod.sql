5d0fededc6b27b4a36538d4c5e57e8406459c921
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-07 05:42:25 +0700

    Update sp_FindInboundOrdersByTimePeriod.sql

--- sp_inboundOrder/sp_FindInboundOrdersByTimePeriod.sql
+++ sp_inboundOrder/sp_FindInboundOrdersByTimePeriod.sql
@@ -5,21 +5,26 @@ AS
 BEGIN
     IF @start_date IS NULL AND @end_date IS NULL
 	BEGIN
-		SELECT * FROM Inbound_Order
+		SELECT IO.inbound_order_id, M.manufacturer_name, IO.order_date, IO.total_price, IO.status
+		FROM Inbound_Order IO, Manufacturer M
+		WHERE IO.manufacturer_id = M.manufacturer_id
 	END;
 	ELSE IF @start_date IS NULL AND @end_date IS NOT NULL
 	BEGIN
-		SELECT * FROM Inbound_Order
-		WHERE order_date <= @end_date
+		SELECT IO.inbound_order_id, M.manufacturer_name, IO.order_date, IO.total_price, IO.status
+		FROM Inbound_Order IO, Manufacturer M
+		WHERE IO.manufacturer_id = M.manufacturer_id AND IO.order_date <= @end_date
 	END;
 	ELSE IF @start_date IS NOT NULL AND @end_date IS NULL
 	BEGIN
-		SELECT * FROM Inbound_Order
-		WHERE order_date >= @start_date
+		SELECT IO.inbound_order_id, M.manufacturer_name, IO.order_date, IO.total_price, IO.status
+		FROM Inbound_Order IO, Manufacturer M
+		WHERE IO.manufacturer_id = M.manufacturer_id AND IO.order_date >= @start_date
 	END;
 	ELSE
 	BEGIN
-		SELECT * FROM Inbound_Order
-		WHERE order_date BETWEEN @start_date AND @end_date
+		SELECT IO.inbound_order_id, M.manufacturer_name, IO.order_date, IO.total_price, IO.status
+		FROM Inbound_Order IO, Manufacturer M
+		WHERE IO.manufacturer_id = M.manufacturer_id AND IO.order_date BETWEEN @start_date AND @end_date
 	END;
 END;
