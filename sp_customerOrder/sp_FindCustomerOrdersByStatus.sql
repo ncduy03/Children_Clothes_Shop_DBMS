8c8770e02e1e913247e86cb64c2d2b3abf1e7b28
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-07 05:27:40 +0700

    Update sp_FindCustomerOrdersByStatus.sql

--- sp_customerOrder/sp_FindCustomerOrdersByStatus.sql
+++ sp_customerOrder/sp_FindCustomerOrdersByStatus.sql
@@ -4,11 +4,14 @@ AS
 BEGIN
     IF @status IS NULL
 	BEGIN
-		SELECT * FROM Customer_Order
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO, Customer C
+		WHERE CO.customer_id = C.customer_id
 	END;
 	ELSE
 	BEGIN
-		SELECT * FROM Customer_Order
-		WHERE status LIKE '%' + @status + '%';
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO, Customer C
+		WHERE status LIKE '%' + @status + '%' AND CO.customer_id = C.customer_id
 	END;
 END;
