5fc76ae61eb441e9838efccd06bea2730413510b
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-07 23:50:49 +0700

    Update UpdateCustomerOrderTotalPrice.sql

--- sp_customerOrder/UpdateCustomerOrderTotalPrice.sql
+++ sp_customerOrder/UpdateCustomerOrderTotalPrice.sql
@@ -1,13 +1,12 @@
 CREATE OR ALTER TRIGGER UpdateOrderTotalPrice
 ON Customer_Order_Detail
-FOR INSERT, UPDATE, DELETE
+FOR INSERT, UPDATE
 AS
 BEGIN
     UPDATE CO
-    SET CO.total_price = S 
-	FROM Customer_Order CO, (SELECT COD.customer_order_id ,SUM(COD.price) as S
-						FROM Customer_Order_Detail COD, inserted I
-						WHERE COD.customer_order_id = I.customer_order_id
-						GROUP BY COD.customer_order_id) AS T
+    SET CO.total_price = T.S + CO.total_price
+	FROM Customer_Order CO, (SELECT I.customer_order_id ,SUM(I.price) as S
+								FROM inserted I
+								GROUP BY I.customer_order_id) AS T
 	WHERE CO.customer_order_id = T.customer_order_id
 END;
