8cf9975d85f1342f5b54973c74128f32af73372f
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-08 15:01:45 +0700

    Update UpdateCustomerOrderTotalPrice.sql

--- sp_customerOrder/UpdateCustomerOrderTotalPrice.sql
+++ sp_customerOrder/UpdateCustomerOrderTotalPrice.sql
@@ -4,9 +4,10 @@ FOR INSERT, UPDATE
 AS
 BEGIN
     UPDATE CO
-    SET CO.total_price = T.S + CO.total_price
-	FROM Customer_Order CO, (SELECT I.customer_order_id ,SUM(I.price) as S
-								FROM inserted I
-								GROUP BY I.customer_order_id) AS T
+    SET CO.total_price = T.S
+	FROM Customer_Order CO, (SELECT COD.customer_order_id ,SUM(COD.price) as S
+								FROM Customer_Order_Detail COD, (SELECT DISTINCT(customer_order_id) FROM inserted) AS I
+								WHERE I.customer_order_id = COD.customer_order_id
+								GROUP BY COD.customer_order_id) AS T
 	WHERE CO.customer_order_id = T.customer_order_id
 END;
