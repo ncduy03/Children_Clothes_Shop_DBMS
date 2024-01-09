53d573f1e5460459c1d248d4bebeb8b39d262764
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-09 13:54:29 +0700

    Update insert_customer_order_detail.sql

--- Children Clothes Shop DBMS/insert_customer_order_detail.sql
+++ Children Clothes Shop DBMS/insert_customer_order_detail.sql
@@ -976,3 +976,13 @@ VALUES
 (974, 974, 6),
 (975, 975, 8),
 (976, 976, 5);
+
+BULK INSERT Customer_Order_Detail
+FROM 'C:\Users\nhhoa\Documents\SQL Server Management Studio\Children Clothes Shop DBMS\customer_order_detail.txt'
+WITH
+	(
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '\n',
+	CODEPAGE = '65001',
+	FIRE_TRIGGERS
+	)
