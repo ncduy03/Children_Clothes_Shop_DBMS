58bc88a1fcf1b4e0942cd35731306e37c29a5aa5
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-11 10:03:23 +0700

    Add files via upload

---
+++ sp_customerOrder/CustomerOrderCancelled.sql
@@ -0,0 +1,13 @@
+﻿CREATE OR ALTER TRIGGER CustomerOrderCancelled
+ON Customer_Order
+FOR UPDATE
+AS
+BEGIN
+	IF UPDATE(status)
+	BEGIN
+		DELETE FROM COD
+        FROM Customer_Order_Detail AS COD
+        INNER JOIN inserted AS I ON COD.customer_order_id = I.customer_order_id
+        WHERE I.status = N'Đã hủy';
+	END;
+END;
