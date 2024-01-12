bd36807a650042945784935692af49a7fbeb461e
HoangNH133 <HoangNH133@fpt.com>
2024-01-12 23:17:53 +0700

    find by name

--- sp_product/sp_FindProductsByName.sql
+++ sp_product/sp_FindProductsByName.sql
@@ -1,16 +1,15 @@
-CREATE OR ALTER PROCEDURE FindProductsByCategoryName
-    @category_name NVARCHAR(50) = NULL
+CREATE OR ALTER PROCEDURE FindProductsByName
+    @name NVARCHAR(50) = NULL
 AS
 BEGIN
-    IF @category_name IS NULL
+    IF @name IS NULL
 	BEGIN
 		SELECT * FROM Product
 	END;
 	ELSE
 	BEGIN
-		SELECT P.*
-		FROM Product P
-		INNER JOIN Product_category PC ON P.product_category_id = PC.product_category_id
-		WHERE PC.category_name LIKE '%' + @category_name + '%';
+		SELECT *
+		FROM Product 
+		WHERE name LIKE '%' + @name + '%';
 	END;
 END;
