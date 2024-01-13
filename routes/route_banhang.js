0af03ce1c81473d8c3c68eb30675fb7e39f5ec17
Ttdne <86353412+Ttdne@users.noreply.github.com>
2024-01-14 04:04:10 +0700

    hoàn thành chức năng tìm kiếm trong đơn hàng

--- routes/route_banhang.js
+++ routes/route_banhang.js
@@ -15,39 +15,71 @@ sql.connect(config);
 const router = express.Router()
 
 
-router.post("/banhang", async (req, res) => {
-    const request = new sql.Request();
-    const inputData1 = req.body.input_data1;
-    const inputData2 = req.body.input_data2;
-    request.input('InputData1', sql.NVarChar, inputData1);
-    request.input('InputData2', sql.NVarChar, inputData2);
-    const result = await request.query(`EXEC FindCustomerOrdersByTimePeriod @InputData1, @InputData2`);
-    res.render('banhang', { dulieu: result.recordset });
-
-});
-router.post('/nhaphang/add', async (req, res) => {
+router.post('/banhang/add', async (req, res) => {
     try {
         const id = req.body.ma;
         const TrangThai = req.body.trangthai;
         const Nhanvien = req.body.ma_nhanvien;
         const request = new sql.Request();
         request.input('ma', sql.Int, id);
-        request.input('Status', sql.NVarChar, TrangThai);
+        request.input('trangthai', sql.NVarChar, TrangThai);
         request.input('nhanvien', sql.Int, Nhanvien);
         const result = await request.query(`
     EXEC AddCustomerOrder 
-    @customer_id=@id,
-    @status=@Status,
+    @customer_id=@ma,
+    @status=@trangthai,
     @employee_id=@nhanvien
 `);
         if (result) console.log("Trueeee");
-        const result1 = await sql.query(`SELECT * FROM Inbound_Order`);
-        res.render('nhaphang', { dulieu: result1.recordset });
+        const result1 = await sql.query(`SELECT TOP 10 c.name, co.* FROM Customer c JOIN Customer_Order co ON c.customer_id = co.customer_id`);
+        res.render('banhang', { dulieu: result1.recordset });
     }
     catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
+});
+
+// router.post('/banhang/check1', async (req, res) => {
+//     const result = await sql.query(`SELECT TOP 100 c.name, co.* FROM Customer c JOIN Customer_Order co ON c.customer_id = co.customer_id WHERE co.status=N'Đang xử lí'`);
+//     res.render('banhang', { dulieu: result.recordset });
+// });
+router.post('/banhang', async (req, res) => {
+    const request = new sql.Request();
+    const check1 = req.body.checked1;
+    const check2 = req.body.checked2;
+    const check3 = req.body.checked3;
+    const inputData1 = req.body.input_data1;
+    const inputData2 = req.body.input_data2;
+    var Dk1 = null;
+    if (check1) Dk1 = 'Đang xử lí';
+    if (check2) Dk1 = 'Đang giao';
+    if (check3) Dk1 = 'Đã giao';
+    request.input('dk1', sql.NVarChar, Dk1);
+    request.input('InputData1', sql.NVarChar, inputData1);
+    request.input('InputData2', sql.NVarChar, inputData2);
+    console.log(inputData2);
+
+    if (inputData1 && inputData2) {
+        const result = await request.query(`EXEC FindCustomerOrdersByStatusAndDatetime @dk1, @InputData1, @InputData2`);
+        res.render('banhang', { dulieu: result.recordset });
+
+    }
+    else if (inputData1 && !inputData2) {
+        const result = await request.query(`EXEC FindCustomerOrdersByStatusAndDatetime @dk1, @InputData1, NULL`);
+        res.render('banhang', { dulieu: result.recordset });
+    }
+    else if (!inputData1 && inputData2) {
+        const result = await request.query(`EXEC FindCustomerOrdersByStatusAndDatetime @dk1, NULL, @InputData2`);
+        res.render('banhang', { dulieu: result.recordset });
+    }
+    else {
+        const result1 = await request.query(`EXEC FindCustomerOrdersByStatusAndDatetime @dk1, NULL, NULL`);
+        res.render('banhang', { dulieu: result1.recordset });
+
+
+    }
+
 });
 export default router;
 
--- sp_customerOrder/FindCustomerOrdersByStatusAndDatetime.sql
+++ sp_customerOrder/FindCustomerOrdersByStatusAndDatetime.sql
@@ -1,10 +1,10 @@
-CREATE OR ALTER PROCEDURE FindCustomerOrdersByStatusAndDatetime
+﻿CREATE OR ALTER PROCEDURE FindCustomerOrdersByStatusAndDatetime
     @status NVARCHAR(50) = NULL,
     @start_time DATE = NULL,
     @end_time DATE = NULL
 AS
 BEGIN
-    SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+    SELECT TOP 100 CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
     FROM Customer_Order CO
     INNER JOIN Customer C ON CO.customer_id = C.customer_id
     WHERE
@@ -13,3 +13,5 @@ BEGIN
         (@end_time IS NULL OR CO.order_date <= @end_time)
 	ORDER BY CO.customer_order_id
 END;
+
+
