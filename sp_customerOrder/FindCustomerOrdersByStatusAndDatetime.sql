b7589f4684b553d3f2d979959859910aa65a69f3
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-15 18:32:48 +0700

    Update FindCustomerOrdersByStatusAndDatetime.sql

--- sp_customerOrder/FindCustomerOrdersByStatusAndDatetime.sql
+++ sp_customerOrder/FindCustomerOrdersByStatusAndDatetime.sql
@@ -1,17 +1,75 @@
-﻿CREATE OR ALTER PROCEDURE FindCustomerOrdersByStatusAndDatetime
+CREATE OR ALTER PROCEDURE FindCustomerOrdersByStatusAndDatetime
     @status NVARCHAR(50) = NULL,
     @start_time DATE = NULL,
     @end_time DATE = NULL
 AS
 BEGIN
-    SELECT TOP 100 CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
-    FROM Customer_Order CO
-    INNER JOIN Customer C ON CO.customer_id = C.customer_id
-    WHERE
-        (@status IS NULL OR CO.status = @status) AND
-        (@start_time IS NULL OR CO.order_date >= @start_time) AND
-        (@end_time IS NULL OR CO.order_date <= @end_time)
-	ORDER BY CO.customer_order_id
+    IF @status IS NOT NULL AND @start_time IS NOT NULL AND @end_time IS NOT NULL
+	BEGIN
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO
+		INNER JOIN Customer C ON CO.customer_id = C.customer_id
+		WHERE status = @status AND order_date BETWEEN @start_time AND @end_time
+		ORDER BY CO.order_date
+	END;
+	ELSE IF @status IS NOT NULL AND @start_time IS NULL AND @end_time IS NOT NULL
+	BEGIN
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO
+		INNER JOIN Customer C ON CO.customer_id = C.customer_id
+		WHERE status = @status AND order_date <= @end_time
+		ORDER BY CO.order_date
+	END;
+	ELSE IF @status IS NOT NULL AND @start_time IS NOT NULL AND @end_time IS NULL
+	BEGIN
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO
+		INNER JOIN Customer C ON CO.customer_id = C.customer_id
+		WHERE
+			status = @status AND order_date >= @start_time
+		ORDER BY CO.order_date
+	END;
+	ELSE IF @status IS NOT NULL AND @start_time IS NULL AND @end_time IS NULL
+	BEGIN
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO
+		INNER JOIN Customer C ON CO.customer_id = C.customer_id
+		WHERE
+			status = @status
+		ORDER BY CO.order_date
+	END;
+	ELSE IF @status IS NULL AND @start_time IS NOT NULL AND @end_time IS NOT NULL
+	BEGIN
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO
+		INNER JOIN Customer C ON CO.customer_id = C.customer_id
+		WHERE
+			order_date BETWEEN @start_time AND @end_time
+		ORDER BY CO.order_date
+	END;
+	ELSE IF @status IS NULL AND @start_time IS NULL AND @end_time IS NOT NULL
+	BEGIN
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO
+		INNER JOIN Customer C ON CO.customer_id = C.customer_id
+		WHERE
+			order_date <= @end_time
+		ORDER BY CO.order_date
+	END;
+	ELSE IF @status IS NULL AND @start_time IS NOT NULL AND @end_time IS NULL
+	BEGIN
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO
+		INNER JOIN Customer C ON CO.customer_id = C.customer_id
+		WHERE
+			order_date >= @start_time
+		ORDER BY CO.order_date
+	END;
+	ELSE IF @status IS NULL AND @start_time IS NULL AND @end_time IS NULL
+	BEGIN
+		SELECT CO.customer_order_id, C.name, CO.order_date, CO.total_price, CO.status
+		FROM Customer_Order CO
+		INNER JOIN Customer C ON CO.customer_id = C.customer_id
+		ORDER BY CO.order_date
+	END;
 END;
-
-
