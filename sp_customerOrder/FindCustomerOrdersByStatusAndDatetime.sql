c5c7edd5e0dcfab43a9d268e43df5dc27070fdbb
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-17 01:11:21 +0700

    Order by DESC

--- sp_customerOrder/FindCustomerOrdersByStatusAndDatetime.sql
+++ sp_customerOrder/FindCustomerOrdersByStatusAndDatetime.sql
@@ -10,7 +10,7 @@ BEGIN
 		FROM Customer_Order CO
 		INNER JOIN Customer C ON CO.customer_id = C.customer_id
 		WHERE status = @status AND order_date BETWEEN @start_time AND DATEADD(DAY, 2, @end_time)
-		ORDER BY CO.order_date
+		ORDER BY CO.order_date DESC
 	END;
 	ELSE IF @status IS NOT NULL AND @start_time IS NULL AND @end_time IS NOT NULL
 	BEGIN
@@ -18,7 +18,7 @@ BEGIN
 		FROM Customer_Order CO
 		INNER JOIN Customer C ON CO.customer_id = C.customer_id
 		WHERE status = @status AND order_date <= DATEADD(DAY, 2, @end_time)
-		ORDER BY CO.order_date
+		ORDER BY CO.order_date DESC
 	END;
 	ELSE IF @status IS NOT NULL AND @start_time IS NOT NULL AND @end_time IS NULL
 	BEGIN
@@ -27,7 +27,7 @@ BEGIN
 		INNER JOIN Customer C ON CO.customer_id = C.customer_id
 		WHERE
 			status = @status AND order_date >= @start_time
-		ORDER BY CO.order_date
+		ORDER BY CO.order_date DESC
 	END;
 	ELSE IF @status IS NOT NULL AND @start_time IS NULL AND @end_time IS NULL
 	BEGIN
@@ -36,7 +36,7 @@ BEGIN
 		INNER JOIN Customer C ON CO.customer_id = C.customer_id
 		WHERE
 			status = @status
-		ORDER BY CO.order_date
+		ORDER BY CO.order_date DESC
 	END;
 	ELSE IF @status IS NULL AND @start_time IS NOT NULL AND @end_time IS NOT NULL
 	BEGIN
@@ -45,7 +45,7 @@ BEGIN
 		INNER JOIN Customer C ON CO.customer_id = C.customer_id
 		WHERE
 			order_date BETWEEN @start_time AND DATEADD(DAY, 2, @end_time)
-		ORDER BY CO.order_date
+		ORDER BY CO.order_date DESC
 	END;
 	ELSE IF @status IS NULL AND @start_time IS NULL AND @end_time IS NOT NULL
 	BEGIN
@@ -54,7 +54,7 @@ BEGIN
 		INNER JOIN Customer C ON CO.customer_id = C.customer_id
 		WHERE
 			order_date <= DATEADD(DAY, 2, @end_time)
-		ORDER BY CO.order_date
+		ORDER BY CO.order_date DESC
 	END;
 	ELSE IF @status IS NULL AND @start_time IS NOT NULL AND @end_time IS NULL
 	BEGIN
@@ -63,13 +63,13 @@ BEGIN
 		INNER JOIN Customer C ON CO.customer_id = C.customer_id
 		WHERE
 			order_date >= @start_time
-		ORDER BY CO.order_date
+		ORDER BY CO.order_date DESC
 	END;
 	ELSE IF @status IS NULL AND @start_time IS NULL AND @end_time IS NULL
 	BEGIN
 		SELECT CO.customer_order_id, C.name, CO.order_date, FORMAT(CO.total_price, 'N0') AS TP, CO.status
 		FROM Customer_Order CO
 		INNER JOIN Customer C ON CO.customer_id = C.customer_id
-		ORDER BY CO.order_date
+		ORDER BY CO.order_date DESC
 	END;
 END;
