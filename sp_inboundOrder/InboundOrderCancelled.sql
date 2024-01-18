b19c30212176129f7541a4e52cac8ebb748a03d2
nhhsag12 <122380669+nhhsag12@users.noreply.github.com>
2024-01-18 15:49:15 +0700

    Add files via upload

---
+++ sp_inboundOrder/InboundOrderCancelled.sql
@@ -0,0 +1,14 @@
+﻿CREATE OR ALTER TRIGGER InboundOrderCancelled
+ON Inbound_Order
+FOR UPDATE
+AS
+BEGIN
+	IF UPDATE(status)
+	BEGIN
+		DELETE FROM IOD
+        FROM Inbound_Order_Detail AS IOD
+        INNER JOIN inserted AS I ON IOD.inbound_order_id = I.inbound_order_id
+        WHERE I.status = N'Đã hủy';
+	END;
+END;
+
