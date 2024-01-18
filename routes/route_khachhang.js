e7063290a145a2ce369ca19859d0397a90646efd
Ttdne <86353412+Ttdne@users.noreply.github.com>
2024-01-19 00:01:40 +0700

    Update route_khachhang.js

--- routes/route_khachhang.js
+++ routes/route_khachhang.js
@@ -25,7 +25,7 @@ router.post('/khachhang/add', async (req, res) => {
     @customer_name = @name, 
     @phone = @SDT`);
     if (result) console.log("Trueeee");
-    const result1 = await sql.query(`SELECT * FROM Customer`);
+    const result1 = await sql.query(`SELECT * FROM Customer ORDER BY customer_id DESC`);
     res.render('khachhang', { dulieu: result1.recordset });
 })
 
@@ -59,5 +59,47 @@ router.post("/khachhang/xoa", async (req, res) => {
     const result1 = await sql.query(`SELECT * FROM Customer`);
     res.render('khachhang', { dulieu: result1.recordset });
 })
+
+router.post('/khachhang/chinhsua', async (req, res) => {
+    try {
+        const customerId = req.body.customerId;
+        const newName = req.body.newName;
+        const newPhone = req.body.newPhone;
+
+        const request = new sql.Request();
+        request.input('customer_id', sql.NVarChar, customerId);
+        request.input('new_name', sql.NVarChar, newName);
+        request.input('new_phone', sql.NVarChar, newPhone);
+
+        const result = await request.query(`
+            EXEC UpdateCustomer 
+            @customer_id = @customer_id,
+            @customer_name = @new_name,
+            @phone = @new_phone`
+        );
+
+        if (result) console.log("Customer details updated successfully");
+        const updatedResult = await sql.query(`SELECT * FROM Customer`);
+        res.render('khachhang', { dulieu: updatedResult.recordset });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Route xử lý lấy thông tin khách hàng để hiển thị trong modal
+router.post('/khachhang/chinhsua/info', async (req, res) => {
+    try {
+        const customerId = req.body.customerId;
+        const request = new sql.Request();
+        request.input('customer_id', sql.NVarChar, customerId);
+        const result = await request.query(`SELECT * FROM Customer WHERE customer_id = @customer_id`);
+        res.json(result.recordset[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 export default router;
 
